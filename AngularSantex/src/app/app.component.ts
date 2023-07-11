84a0da6bd1f5be60e87a84ba77b7a019a1c4b9a9
fernandojbcba <benavidez.fernando@hotmail.com>
2023-07-11 19:41:24 -0300

    terminado

--- AngularSantex/src/app/app.component.ts
+++ AngularSantex/src/app/app.component.ts
@@ -16,7 +16,13 @@ export class AppComponent {
   lista: Item[] = [
     { nombre: 'Producto 1', costo: '$10' },
     { nombre: 'Producto 2', costo: '$20' },
-    { nombre: 'Producto 3', costo: '$30' }
+    { nombre: 'Producto 4', costo: '$30' },
+    { nombre: 'Producto 5', costo: '$100' },
+    { nombre: 'Producto 6', costo: '$200' },
+    { nombre: 'Producto 7', costo: '$350' },
+    { nombre: 'Producto 8', costo: '$120' },
+    { nombre: 'Producto 9', costo: '$230' },
+    { nombre: 'Producto 10', costo: '$310' },
   ];
   
   toggleLista() {
